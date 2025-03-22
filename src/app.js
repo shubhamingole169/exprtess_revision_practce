3a0419539eac295714566330f6ffbc2895999652
Shubham Ingole <shubhamingole169@gmail.com>
2025-03-22 08:24:00 +0530

    assignment copy

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import  express  from 'express';
+import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -9,26 +9,33 @@ app.use(cors({
     credentials: true
 }))
 
-
-//seting limit to access usage
-app.use(express.json({limit:"16kb"}))
-
-//somethime url get in encoded form for undestanding those
-app.use(express.urlencoded({extended: true, limit:"16kb"}))
-
-//for store some static file in serer temp basically
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-
-//for setting secure cookies acessing
 app.use(cookieParser())
 
 
-// routes import
-import userRouter from "./routes/user.route.js"
+//routes import
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
-// routes declaration
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
-
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost:8000/api/v1/users/register
 
